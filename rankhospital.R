dea5bc516f489e4a48099874b950efb6cc6d1643
Ivan Lozo <lozo.ivan@gmail.com>
2015-03-22 11:27:39 +0100

    rank hospital

--- rankhospital.R
+++ rankhospital.R
@@ -3,6 +3,7 @@ rankhospital<-function(state,outcome, num) {
 #read outcome 
 data<-read.table("outcome-of-care-measures.csv",dec = ".",header = TRUE,sep = ",",colClasses = "character")
 
+ranknum<-as.numeric()
 
 #Check that state and outcome are valid
 if (nrow(data[data$State== state,]) == 0) 
@@ -19,7 +20,28 @@ suppressWarnings(statedata[, 4] <- as.numeric(statedata[,4]))
 suppressWarnings(statedata[, 5] <- as.numeric(statedata[,5]))
 suppressWarnings(statedata[, 3] <- as.numeric(statedata[,3]))
 
-#New data frame with ordered data
-newstatedata<-statedata[order(statedata[,outcome]),]
-newstatedata[num,c("Hospital.Name","State", outcome)]
+#Change column names
+colnames(statedata)[3]<-"heart attack"
+colnames(statedata)[4]<-"heart failure"
+colnames(statedata)[5]<-"pneumonia"
+
+#take only name and 
+
+statedata<-statedata[,c("Hospital.Name", outcome)]
+
+#remove NAs
+statedata<-na.omit(statedata)
+
+#define rank value
+if (num=="best") ranknum<-1
+else if (num=="worst") ranknum<-nrow(statedata)
+else if (num > nrow(statedata)) return ("NA")
+else ranknum<-num
+
+#order data
+newstatedata<-statedata[order(statedata[,outcome],statedata$Hospital.Name), ]
+
+#return hospital name according to rank
+return (newstatedata[ranknum,"Hospital.Name"])
 }
+
